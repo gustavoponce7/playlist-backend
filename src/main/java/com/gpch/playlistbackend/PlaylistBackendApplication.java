d16ae1337d9cb41ea6c1a91a24290e3934434080
Gustavo Ponce <gustavo_ponce@epam.com>
2018-09-19 16:53:42 +0300

    First commit

---
+++ src/main/java/com/gpch/playlistbackend/PlaylistBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gpch.playlistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistBackendApplication.class, args);
+    }
+}
